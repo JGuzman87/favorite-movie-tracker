4a0c6912fcb6d8464306851ad80910d4d1ed7978
JGuzman87 <jguzman913@gmail.com>
2025-11-10 11:07:16 -0500

    added handleSubmit

--- src/components/MovieForm.jsx
+++ src/components/MovieForm.jsx
@@ -7,7 +7,7 @@ const MovieForm = ({ id }) => {
     title: "",
     year: "",
     genre: "",
-    favorite: false,
+    favorite: false
   });
 
   const handleChange = (e) => {
@@ -16,9 +16,21 @@ const MovieForm = ({ id }) => {
     setMovie((prev) => ({...prev, [name]: value}) )
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(movie)
+    setMovie({
+      title: "",
+      year: "",
+      genre: "",
+    });
+
+  }
+
   return (
     <>
-      <form className={styles.form} onSubmit={""}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         <label htmlFor="title">Title: </label>
         <input type="text" name="title" value={movie.title} onChange={handleChange} />
         <label htmlFor="year">Year: </label>
